8e5b707a3bec41da918e6f269d0c1fc8d1e41a03
mrschwabe <admin@mrschwabe.com>
2013-04-02 22:24:20 -0600

    Established project DICE.

---
+++ dice.rb
@@ -0,0 +1,24 @@
+=begin 
+#########
+This is a practice Ruby script.  To simulate... a DICE. 
+What can a dice do?  Well. It can roll. 
+What properties are of it?  It has 6 sides (typically, though some DnD ones have more)
+What happens when dice rolled?  It lands!  One one of the six sides.
+The dice then shows the result.  And the roll is complete.  
+So we have a begin state (let's assume begin state is in your hand).
+A rolling state.  Rolls typically take a few seconds. 
+And the final result state.  In which the roll is complete, and the result is displayed for us all to see. 
+#########	
+=end
+
+#ai 
+##accepts 1 parameter.  Tells the function whether or not it was called by the input function or the display function. 
+
+#display
+##renders stuff to the screen
+
+#ai (we loop through it)
+
+#input
+##accepts user input
+
